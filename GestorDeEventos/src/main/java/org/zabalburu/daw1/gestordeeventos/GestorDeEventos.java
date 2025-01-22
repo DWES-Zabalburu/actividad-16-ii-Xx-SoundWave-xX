8e9ed9cdc4879a14449da2bd7e1721dc4fdcd7e0
Xx-SoundWave-xX <fernandojohel.lara.mantilla@zabalburu.org>
2025-01-22 09:30:57 +0100

    Gesto De Eventos(v1)

---
+++ GestorDeEventos/src/main/java/org/zabalburu/daw1/gestordeeventos/GestorDeEventos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.zabalburu.daw1.gestordeeventos;
+
+/**
+ *
+ * @author DAW1
+ */
+public class GestorDeEventos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
